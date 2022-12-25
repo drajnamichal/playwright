8ebe964f7b9d798990b3ac3e748d4a99e2934d10
Michal Drajna <40984942+drajnamichal@users.noreply.github.com>
2022-12-25 14:51:02 +0100

    Update graphQLPostRequestUntilCondition.ts

--- graphQLPostRequestUntilCondition.ts
+++ graphQLPostRequestUntilCondition.ts
@@ -1,4 +1,20 @@
-    // refresh the page, send request to orders api and verify status
+// get cookies
+    const obj = await context.cookies();
+// get access token
+    const tokenObject = obj.filter((val) => val.name.includes('idToken'));
+    const token = tokenObject.map((a) => a.value).toString();
+
+// submit investment and wait for response
+    const requestPromise = page.waitForRequest(
+      (req) => req.url().includes('/orders/api/graphql') && req.method() === 'POST'
+    );
+    xyzPage.submit();
+    page.waitForResponse((resp) => resp.url().includes('/orders/api/graphql') && resp.status() === 200);
+    // get transactionId
+    const investRequest = await requestPromise;
+    const transactionId = investRequest.postDataJSON().variables.input.transactionId;
+
+// refresh the page, send request to orders api and verify status
     await expect
       .poll(
         async () => {
