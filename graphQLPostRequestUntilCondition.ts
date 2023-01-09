4d8084ba199fdea2defdfdd4f9631952dd0519fa
Michal Drajna <40984942+drajnamichal@users.noreply.github.com>
2023-01-09 11:49:39 +0100

    Update graphQLPostRequestUntilCondition.ts

--- graphQLPostRequestUntilCondition.ts
+++ graphQLPostRequestUntilCondition.ts
@@ -43,3 +43,10 @@
       .toBeTruthy();
 
 // continue
+
+
+// This code uses the Playwright library to send a GraphQL POST request to the /orders/api/graphql endpoint. It first gets the cookies from the context and extracts the idToken cookie value, which it uses as an authorization token in the request headers.
+
+// The code then waits for a request to the /orders/api/graphql endpoint with a POST method and extracts the transactionId from the request's postDataJSON. It then refreshes the page and sends a new request to the /orders/api/graphql endpoint using the request object.
+
+// This new request queries the Order field with the transactionId and waits for the status field to not be InProgress. If the status field is not InProgress within 60 seconds, the poll function returns true. Otherwise, it will continue to poll until the timeout is reached.
