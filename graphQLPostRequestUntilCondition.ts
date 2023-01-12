cb3e6989e4b4f45a51cd0511ff4cf36d89f04b69
Michal Drajna <40984942+drajnamichal@users.noreply.github.com>
2023-01-12 10:59:09 +0100

    Update graphQLPostRequestUntilCondition.ts

--- graphQLPostRequestUntilCondition.ts
+++ graphQLPostRequestUntilCondition.ts
@@ -6,23 +6,23 @@
 
 // submit investment and wait for response
     const requestPromise = page.waitForRequest(
-      (req) => req.url().includes('/orders/api/graphql') && req.method() === 'POST'
+      (req) => req.url().includes('/invoice/api/graphql') && req.method() === 'POST'
     );
-    xyzPage.submit();
-    page.waitForResponse((resp) => resp.url().includes('/orders/api/graphql') && resp.status() === 200);
+    yourPage.submit();
+    page.waitForResponse((resp) => resp.url().includes('/invoice/api/graphql') && resp.status() === 200);
     // get transactionId
-    const investRequest = await requestPromise;
-    const transactionId = investRequest.postDataJSON().variables.input.transactionId;
+    const invoiceRequest = await requestPromise;
+    const transactionId = invoiceRequest.postDataJSON().variables.input.transactionId;
 
 // refresh the page, send request to orders api and verify status
     await expect
       .poll(
         async () => {
           await page.reload();
-          const ordersResponse = await request.post('/orders/api/graphql', {
+          const invoiceResponse = await request.post('/invoice/api/graphql', {
             data: {
               query:
-                'query GetOrderInfo($transactionId: ID!) { Order(transactionId: $transactionId) { status, unsuccessOrFailReasons } }',
+                'query GetOrderInfo($transactionId: ID!) { Invoice(transactionId: $transactionId) { status, unsuccessOrFailReasons } }',
               variables: {
                 transactionId: `${transactionId}`,
               },
@@ -31,9 +31,9 @@
               authorization: `Bearer ${token}`,
             },
           });
-          const jsonData = await ordersResponse.json();
-          const orderStatus = jsonData.data.Order.status;
-          return orderStatus !== 'InProgress';
+          const jsonData = await invoiceResponse.json();
+          const invoiceStatus = jsonData.data.Invoice.status;
+          return invoiceStatus !== 'InProgress';
         },
         {
           // Poll for 60 seconds
@@ -43,10 +43,3 @@
       .toBeTruthy();
 
 // continue
-
-
-// This code uses the Playwright library to send a GraphQL POST request to the /orders/api/graphql endpoint. It first gets the cookies from the context and extracts the idToken cookie value, which it uses as an authorization token in the request headers.
-
-// The code then waits for a request to the /orders/api/graphql endpoint with a POST method and extracts the transactionId from the request's postDataJSON. It then refreshes the page and sends a new request to the /orders/api/graphql endpoint using the request object.
-
-// This new request queries the Order field with the transactionId and waits for the status field to not be InProgress. If the status field is not InProgress within 60 seconds, the poll function returns true. Otherwise, it will continue to poll until the timeout is reached.
